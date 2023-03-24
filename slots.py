595d718bed18fe521a6ab73d77df1862a6c25852
Danison <zhengyzheng21@gmail.com>
2023-03-24 12:37:57 -0500

    while statement and explanation

--- slots.py
+++ slots.py
@@ -1,6 +1,10 @@
 # checks if bet amount is valid
 total = input("How much do you want to add to your balance? ")
 
+# print statement describing game and different bets
+print("")
+
 # ask player to put money into slot machine
-while amount >= total:
-    amount = input("How much do you want to put in? ")
+amount = input("How much do you want to put into the slot machine? ")
+while amount > total:
+    amount = input("You can not exceed $" + total + ". Enter different amount: ")
