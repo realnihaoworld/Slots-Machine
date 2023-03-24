7854d138dd6b28e02ee6755c69e4ecf93fd39c5b
Danison <zhengyzheng21@gmail.com>
2023-03-24 15:00:17 -0500

    int stuff

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-import random as rnd
+
--- slots.py
+++ slots.py
@@ -1,10 +1,17 @@
+# emoji list
+icons = ['🍎', '🍍', '🎰', '🐍', '⚜', '🎉', '💎', '🏆', '♥', '💰']
+
 # checks if bet amount is valid
-total = input("How much do you want to add to your balance? ")
+total = int(input("How much do you want to add to your balance? "))
 
 # print statement describing game and different bets
-print("")
+print("Welcome to my slot machine! Here you can make normal rolls ($0.50) or jackpot rolls (all in!)")
+print("---------------------------------------------------------------------------------------------")
 
 # ask player to put money into slot machine
-amount = input("How much do you want to put into the slot machine? ")
+amount = int(input("How much do you want to put into the slot machine? "))
+
 while amount > total:
-    amount = input("You can not exceed $" + total + ". Enter different amount: ")
+    amount = int(input(f"You can not exceed ${total}. Enter different amount: "))
+
+betType = input("Do you want a normal (n) or jackpot roll? (j) ")
