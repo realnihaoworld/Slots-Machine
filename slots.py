7f59471c471af4a5761995b9608f38fdb560ea4a
Danison <zhengyzheng21@gmail.com>
2023-03-27 13:35:05 -0500

    added random function
    yeyey

--- slots.py
+++ slots.py
@@ -1,17 +1,34 @@
+import random as rnd
+
 # emoji list
-icons = ['🍎', '🍍', '🎰', '🐍', '⚜', '🎉', '💎', '🏆', '♥', '💰']
+symbols = ['🍎', '🍍', '🎰', '🐍', '⚜', '🎉', '💎', '🏆', '♥', '💰']
 
-# checks if bet amount is valid
-total = int(input("How much do you want to add to your balance? "))
+def main():
 
-# print statement describing game and different bets
-print("Welcome to my slot machine! Here you can make normal rolls ($0.50) or jackpot rolls (all in!)")
-print("---------------------------------------------------------------------------------------------")
+    # checks if bet amount is valid
+    total = int(input("How much do you want to add to your balance? "))
 
-# ask player to put money into slot machine
-amount = int(input("How much do you want to put into the slot machine? "))
+    # print statement describing game and different bets
+    print("Welcome to my slot machine! Here you can make normal rolls ($0.50) or jackpot rolls (all in!)")
+    print("---------------------------------------------------------------------------------------------")
 
-while amount > total:
-    amount = int(input(f"You can not exceed ${total}. Enter different amount: "))
+    # ask player to put money into slot machine
+    amount = int(input("How much do you want to put into the slot machine? "))
 
-betType = input("Do you want a normal (n) or jackpot roll? (j) ")
+    while amount > total:
+        amount = int(input(f"You can not exceed ${total}. Enter different amount: "))
+
+    betType = input("Do you want a normal (n) or jackpot roll? (j) ")
+
+    # print(playSlots())
+
+# assigns values to array
+def playSlots():
+    slotIcons = [0] * 5
+    for icons in range(len(slotIcons)):
+        slotIcons[icons] = rnd.randint(1, 20)
+
+    return slotIcons
+
+if __name__ == "__main__":
+    main()
